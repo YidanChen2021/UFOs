5a82b1dcf8dfaff485a1b0b92e52302ec6af45a7
unknown <eden.chen2010@gmail.com>
2022-01-23 19:06:24 -0500

    .

--- Static/js/app.js
+++ Static/js/app.js
@@ -23,3 +23,23 @@ function buildTable(data) {
         );
     });
 }
+
+function handleClick() {
+    let date = d3.select("datetime").property("Value");
+    let filteredData = tableData;
+
+    if (date) {
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+
+    // Rebuild the table using the filtered data.
+    // @Note: if no date was entered, then filteredData will
+    // just be the original tableData.
+    buildTable(filteredData);
+};
+
+// Attach an event to listen for the from button.
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+//Build the table when the page loads
+buildTable(tableData);
